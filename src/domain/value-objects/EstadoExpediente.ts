b64f96a4e65a74e51c44073fd72457d9c0698a43
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-07-27 14:06:34 +0200

    feat(domain): añadir EstadoExpediente

---
+++ src/domain/value-objects/EstadoExpediente.ts
@@ -0,0 +1,22 @@
+/**
+ * CONTRATA IA
+ * ---------------------------------------------------------
+ * Estados posibles del expediente.
+ * ---------------------------------------------------------
+ */
+
+export enum EstadoExpediente {
+
+    BORRADOR = "BORRADOR",
+
+    EN_ESTUDIO = "EN_ESTUDIO",
+
+    PENDIENTE_VALIDACION = "PENDIENTE_VALIDACION",
+
+    VALIDADO = "VALIDADO",
+
+    FINALIZADO = "FINALIZADO",
+
+    ARCHIVADO = "ARCHIVADO"
+
+}
