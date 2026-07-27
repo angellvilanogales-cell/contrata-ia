b738cce79940cd9e2e9b86c3566e2508a5353426
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-07-27 14:03:09 +0200

    feat(domain): añadir EstadoDecision

---
+++ src/domain/decisiones/EstadoDecision.ts
@@ -0,0 +1,18 @@
+/**
+ * CONTRATA IA
+ * Estado de una decisión administrativa.
+ */
+
+export enum EstadoDecision {
+
+  PROPUESTA = "PROPUESTA",
+
+  PENDIENTE_VALIDACION = "PENDIENTE_VALIDACION",
+
+  VALIDADA = "VALIDADA",
+
+  RECHAZADA = "RECHAZADA",
+
+  ANULADA = "ANULADA"
+
+}
