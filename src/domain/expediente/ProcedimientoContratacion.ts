cdbadef0f4984e3a66d3b1ad87105415e2ded5db
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-07-27 14:22:29 +0200

    feat(domain): crear ProcedimientoContratacion
    Defines an enum for various contracting procedures.

---
+++ src/domain/expediente/ProcedimientoContratacion.ts
@@ -0,0 +1,24 @@
+/**
+ * CONTRATA IA
+ * ---------------------------------------------------------
+ * Procedimiento de adjudicación propuesto.
+ * ---------------------------------------------------------
+ */
+
+export enum ProcedimientoContratacion {
+
+    ABIERTO = "ABIERTO",
+
+    ABIERTO_SIMPLIFICADO = "ABIERTO_SIMPLIFICADO",
+
+    ABIERTO_SIMPLIFICADO_ABREVIADO = "ABIERTO_SIMPLIFICADO_ABREVIADO",
+
+    RESTRINGIDO = "RESTRINGIDO",
+
+    NEGOCIADO = "NEGOCIADO",
+
+    DIALOGO_COMPETITIVO = "DIALOGO_COMPETITIVO",
+
+    ASOCIACION_INNOVACION = "ASOCIACION_INNOVACION"
+
+}
