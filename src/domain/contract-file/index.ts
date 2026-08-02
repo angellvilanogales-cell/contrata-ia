3c50ca1e6c363b208e7864b71f34b01fd62e8013
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-02 14:34:00 +0200

    "feat(contract-file): add domain exports"
    This file serves as the entry point for the ContractFile domain, consolidating public exports of the ContractFile aggregate and its associated models.

---
+++ src/domain/contract-file/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Contrata-IA
+ * ---------------------------------
+ * Punto de entrada del dominio ContractFile.
+ *
+ * Este archivo centraliza las exportaciones públicas del agregado
+ * ContractFile y de todos los modelos que lo componen.
+ */
+
+export * from "./ContractFile";
+
+export * from "./models/Expediente";
+export * from "./models/Organizacion";
+export * from "./models/Necesidad";
+export * from "./models/ObjetoContrato";
+export * from "./models/ClasificacionJuridica";
+export * from "./models/Presupuesto";
+export * from "./models/Procedimiento";
+export * from "./models/Licitacion";
+export * from "./models/Solvencia";
+export * from "./models/Criterios";
+export * from "./models/CondicionesEspeciales";
+export * from "./models/Ejecucion";
+export * from "./models/Documentacion";
+export * from "./models/Auditoria";
+export * from "./models/EstadoExpediente";
