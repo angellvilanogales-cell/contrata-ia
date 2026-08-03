b62d72e12508cd087e0c8c3a11620e0b86df1545
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-03 14:13:07 +0200

    Create index.ts for guarantees module exports
    Add index.ts for exporting guarantee-related modules

---
+++ src/domain/rules/guarantees/index.ts
@@ -0,0 +1,28 @@
+/**
+ * ============================================================
+ * CONTRATA-IA
+ * ------------------------------------------------------------
+ * GUARANTEES
+ * ------------------------------------------------------------
+ * Punto único de exportación.
+ *
+ * ============================================================
+ */
+
+export * from "./GuaranteeTypes";
+
+export * from "./GuaranteeCalculationRule";
+
+export * from "./ProvisionalGuaranteeRule";
+
+export * from "./DefinitiveGuaranteeRule";
+
+export * from "./ComplementaryGuaranteeRule";
+
+export * from "./GuaranteeExemptionRule";
+
+export * from "./GuaranteeReturnRule";
+
+export * from "./GuaranteeValidator";
+
+export * from "./GuaranteeResolver";
