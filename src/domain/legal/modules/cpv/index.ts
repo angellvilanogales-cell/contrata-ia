6cc31566f89058def1335dcbbe14bca3550eb5b0
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-05 11:04:52 +0200

    Create CPV module index file for exports

---
+++ src/domain/legal/modules/cpv/index.ts
@@ -0,0 +1,19 @@
+/**
+ * ============================================================
+ * CONTRATA-IA
+ * ------------------------------------------------------------
+ * CPV Module
+ * ------------------------------------------------------------
+ * Exportación única del Motor CPV.
+ * ============================================================
+ */
+
+export * from "./CPVDecision";
+export * from "./CPVContext";
+export * from "./CPVRule";
+export * from "./CPVResult";
+export * from "./CPVTrace";
+export * from "./CPVValidator";
+export * from "./CPVEvaluator";
+export * from "./CPVResolver";
+export * from "./CPVDecisionEngine";
