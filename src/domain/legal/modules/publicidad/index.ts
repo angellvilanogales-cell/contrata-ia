01992844109d27b02c2e63b039489693bbe72f7a
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-05 10:21:55 +0200

    Create index.ts

---
+++ src/domain/legal/modules/publicidad/index.ts
@@ -0,0 +1,20 @@
+/**
+ * ============================================================
+ * CONTRATA-IA
+ * ------------------------------------------------------------
+ * Publicidad Module
+ * ------------------------------------------------------------
+ * Punto único de exportación del Motor de Publicidad.
+ *
+ * ============================================================
+ */
+
+export * from "./PublicityDecision";
+export * from "./PublicityContext";
+export * from "./PublicityRule";
+export * from "./PublicityResult";
+export * from "./PublicityTrace";
+export * from "./PublicityValidator";
+export * from "./PublicityEvaluator";
+export * from "./PublicityResolver";
+export * from "./PublicityDecisionEngine";
