7266bdf67af08b6a3c9f0a32191ea0e3b3603646
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-05 10:50:30 +0200

    Create index.ts for Solvencia module exports
    This file serves as a single export point for various components of the Solvencia module, including decision-making and validation functionalities.

---
+++ src/domain/legal/modules/solvencia/index.ts
@@ -0,0 +1,20 @@
+/**
+ * ============================================================
+ * CONTRATA-IA
+ * ------------------------------------------------------------
+ * Solvencia Module
+ * ------------------------------------------------------------
+ * Punto único de exportación del Motor de Solvencia.
+ *
+ * ============================================================
+ */
+
+export * from "./SolvencyDecision";
+export * from "./SolvencyContext";
+export * from "./SolvencyRule";
+export * from "./SolvencyResult";
+export * from "./SolvencyTrace";
+export * from "./SolvencyValidator";
+export * from "./SolvencyEvaluator";
+export * from "./SolvencyResolver";
+export * from "./SolvencyDecisionEngine";
