5ca5f5c303c19641870e4ace8d3ac6a374265c62
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-05 10:57:12 +0200

    Create Garantías module for guarantee exports
    Add a module for exporting guarantee-related components.

---
+++ src/domain/legal/modules/garantias/index.ts
@@ -0,0 +1,19 @@
+/**
+ * ============================================================
+ * CONTRATA-IA
+ * ------------------------------------------------------------
+ * Garantías Module
+ * ------------------------------------------------------------
+ * Punto único de exportación del Motor de Garantías.
+ * ============================================================
+ */
+
+export * from "./GuaranteeDecision";
+export * from "./GuaranteeContext";
+export * from "./GuaranteeRule";
+export * from "./GuaranteeResult";
+export * from "./GuaranteeTrace";
+export * from "./GuaranteeValidator";
+export * from "./GuaranteeEvaluator";
+export * from "./GuaranteeResolver";
+export * from "./GuaranteeDecisionEngine";
