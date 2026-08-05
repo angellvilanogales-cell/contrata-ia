f0665c0a5896292787c9711a952735fb1b5ed9a8
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-05 10:09:04 +0200

    Create procedimiento module for procedure exports
    Added a module for exporting various procedure-related components.

---
+++ src/domain/legal/modules/procedimiento/index.ts
@@ -0,0 +1,20 @@
+/**
+ * ============================================================
+ * CONTRATA-IA
+ * ------------------------------------------------------------
+ * Procedimiento Module
+ * ------------------------------------------------------------
+ * Punto único de exportación del Motor de Procedimientos.
+ *
+ * ============================================================
+ */
+
+export * from "./ProcedureDecision";
+export * from "./ProcedureContext";
+export * from "./ProcedureRule";
+export * from "./ProcedureResult";
+export * from "./ProcedureTrace";
+export * from "./ProcedureValidator";
+export * from "./ProcedureEvaluator";
+export * from "./ProcedureResolver";
+export * from "./ProcedureDecisionEngine";
