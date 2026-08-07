45af5185a32841f9259a93b7b592d635918992f8
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-07 21:36:31 +0200

    fix(lb3): add runtime composition root

---
+++ src/runtime.ts
@@ -0,0 +1,5 @@
+import { runVerticalDemo, type VerticalFlowResult } from "./application/vertical/VerticalSlice";
+
+export async function executeVerticalDemo(): Promise<VerticalFlowResult> {
+  return runVerticalDemo();
+}
