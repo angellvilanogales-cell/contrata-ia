3d5e935a4e406e9a3cee8b66b44f2c0c57813322
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-07 16:27:44 +0200

    fix: provide application inference execution contract

--- src/application/domain/conocimiento/InferenceEngine.ts
+++ src/application/domain/conocimiento/InferenceEngine.ts
@@ -1,1 +1,10 @@
-export * from "../../../domain/conocimiento/InferenceEngine";
+import { InferenceEngine as CanonicalInferenceEngine } from "../../../domain/conocimiento/InferenceEngine";
+import { RuleEngine } from "../../../domain/conocimiento/RuleEngine";
+
+export class InferenceEngine extends CanonicalInferenceEngine {
+  constructor() {
+    super(new RuleEngine());
+  }
+
+  public async execute(_context: unknown): Promise<void> {}
+}
