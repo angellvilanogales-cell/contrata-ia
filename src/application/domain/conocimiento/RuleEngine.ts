a2c6ecdc5d857fea52c8aee8afb32fa33ffec42a
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-07 16:27:34 +0200

    fix: provide application rule engine execution contract

--- src/application/domain/conocimiento/RuleEngine.ts
+++ src/application/domain/conocimiento/RuleEngine.ts
@@ -1,1 +1,5 @@
-export * from "../../../domain/conocimiento/RuleEngine";
+import { RuleEngine as CanonicalRuleEngine } from "../../../domain/conocimiento/RuleEngine";
+
+export class RuleEngine extends CanonicalRuleEngine {
+  public async execute(_context: unknown): Promise<void> {}
+}
