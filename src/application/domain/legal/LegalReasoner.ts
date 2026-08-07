50c63ebf903376cdb8dd0c6cf9131b90b154162b
angellvilanogales-cell <angellvilanogales@gmail.com>
2026-08-07 16:27:56 +0200

    fix: provide application legal reasoner execution contract

--- src/application/domain/legal/LegalReasoner.ts
+++ src/application/domain/legal/LegalReasoner.ts
@@ -1,1 +1,8 @@
-export * from "../../../domain/legal/LegalReasoner";
+import { LegalReasoner as CanonicalLegalReasoner } from "../../../domain/legal/LegalReasoner";
+import { ContractContextModel } from "../../../application/modules/contract-generator/ContractContext";
+
+export class LegalReasoner extends CanonicalLegalReasoner {
+  public async execute(context: ContractContextModel): Promise<void> {
+    this.initialize(context);
+  }
+}
